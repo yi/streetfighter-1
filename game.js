bbfd87dee13c3a160f81c8864fd84678d368b9d4
Dominic da Costa <dominic.dacosta@derivco.com>
2015-02-09 14:17:45 +0200

    added key binding

--- game.js
+++ game.js
@@ -3,154 +3,249 @@
    //SPRITES ARRAY
    var movies = {};
    var textures = {};
-   var loop = 1,
-     direction = 'forward';
-
-   var framesPerSecond = 12.5;
-   var msPerFrame = 1000 / framesPerSecond;
-   var dirIndex = 1;
 
    //LOAD SPRITESHEET
    var assetsToLoader = ['resources/ken.json', 'resources/shoryuken.json', 'resources/tatsumaki.json'];
-   loader = new PIXI.AssetLoader(assetsToLoader);
+   var loader = new PIXI.AssetLoader(assetsToLoader);
    loader.onComplete = onAssetsLoaded;
    loader.load();
 
    //CREATE STAGE
    var stage = new PIXI.Stage(0x000000);
-   renderer = PIXI.autoDetectRenderer(800, 600);
+   var renderer = PIXI.autoDetectRenderer(384, 224);
    document.body.appendChild(renderer.view);
 
    function onAssetsLoaded() {
 
      //---------------------------------
-
-     textures.stance = [];
-     for (var i = 1; i <= 7; i++) {
-       textures.stance.push('ken' + '_' + pad(i, 2) + '.png');
-     }
-     movies.stance = PIXI.MovieClip.fromFrames(textures.stance);
-
-     //---------------------------------
-
-     textures.punch = [];
-     for (var i = 1; i <= 7; i++) {
-       textures.punch.push('ken' + '_' + pad(i, 2) + '.png');
-     }
-     movies.punch = PIXI.MovieClip.fromFrames(textures.punch);
+     // create a texture from an image path
+     var texture = PIXI.Texture.fromImage('resources/background.png');
+     var bgk = new PIXI.Sprite(texture);
+     bgk.position.x = 0;
+     bgk.position.y = 0;
+     stage.addChild(bgk);
 
      //---------------------------------
 
-     textures.walk = [];
-     for (var i = 1; i <= 7; i++) {
-       textures.walk.push('ken' + '_' + pad(i, 2) + '.png');
-     }
-     movies.walk = PIXI.MovieClip.fromFrames(textures.walk);
+     addMovie('stance', [5, 6, 7, 8], 'ken');
 
-     //---------------------------------
+     addMovie('walk', [13, 15, 16, 17, 18], 'ken');
 
-     textures.kick = [];
-     for (var i = 1; i <= 7; i++) {
-       textures.kick.push('ken' + '_' + pad(i, 2) + '.png');
-     }
-     movies.kick = PIXI.MovieClip.fromFrames(textures.kick);
+     addMovie('punch', [9, 10, 11], 'ken');
 
-     //---------------------------------
+     addMovie('kick', [22, 23, 24, 25], 'ken');
 
-     textures.roundhouse = [];
-     for (var i = 1; i <= 7; i++) {
-       textures.roundhouse.push('ken' + '_' + pad(i, 2) + '.png');
-     }
-     movies.roundhouse = PIXI.MovieClip.fromFrames(textures.roundhouse);
+     addMovie('roundhouse', [26, 27, 28, 29, 30], 'ken');
 
-     //---------------------------------
+     addMovie('crouch', [37], 'ken');
 
-     textures.crouch = [];
-     for (var i = 1; i <= 7; i++) {
-       textures.crouch.push('ken' + '_' + pad(i, 2) + '.png');
-     }
-     movies.crouch = PIXI.MovieClip.fromFrames(textures.crouch);
+     addMovie('jump', [31, 32, 33, 34, 35, 36], 'ken');
 
+     addMovie('fireball', [12, 14], 'ken');
 
-     //---------------------------------
+     addMovie('hadoken', [1, 2, 3, 4], 'ken');
 
-     textures.jump = [];
-     for (var i = 1; i <= 7; i++) {
-       textures.jump.push('ken' + '_' + pad(i, 2) + '.png');
-     }
-     movies.jump = PIXI.MovieClip.fromFrames(textures.jump);
+     addMovie('shoryuken', [1, 2, 3, 4, 5, 6, 7]);
 
-     //---------------------------------
-
-     textures.fireball = [];
-     for (var i = 1; i <= 7; i++) {
-       textures.fireball.push('ken' + '_' + pad(i, 2) + '.png');
-     }
-     movies.fireball = PIXI.MovieClip.fromFrames(textures.fireball);
-
-     //---------------------------------
-
-     textures.hadoken = [];
-     for (var i = 1; i <= 7; i++) {
-       textures.hadoken.push('ken' + '_' + pad(i, 2) + '.png');
-     }
-     movies.hadoken = PIXI.MovieClip.fromFrames(textures.hadoken);
-
-
-     //---------------------------------
-
-     textures.shoryuken = [];
-     for (var i = 1; i <= 7; i++) {
-       textures.shoryuken.push('shoryuken' + pad(i, 2) + '_' + pad(i, 2) + '.png');
-     }
-     movies.shoryuken = PIXI.MovieClip.fromFrames(textures.shoryuken);
-
-     //---------------------------------
-
-     textures.tatsumaki = [];
-     for (var i = 1; i <= 7; i++) {
-       textures.tatsumaki.push('tatsumaki' + pad(i, 2) + '_' + pad(i, 2) + '.png');
-     }
-     movies.tatsumaki = PIXI.MovieClip.fromFrames(textures.tatsumaki);
+     addMovie('tatsumaki', [1, 2, 3, 4, 5, 6, 7]);
 
      //---------------------------------
 
      for (var key in movies) {
        var movie = movies[key];
-       movie.anchor.x = 0.5;
-       movie.anchor.y = 0.5;
-       movie.position.x = 400;
-       movie.position.y = 300;
-       // movie.scale.x *= 2;
-       // movie.scale.y *= 2;
-       movie.stop();
+       movie.anchor.x = 0;
+       movie.anchor.y = 1;
+       movie.visible = false;
+       movie.position.x = 25;
+       movie.position.y = 200;
+       movie.animationSpeed = 10 / 60;
        stage.addChild(movie);
      }
+
+     //---------------------------------
+
+     reset();
+
+     //---------------------------------
+
+     window.onkeydown = commands;
+     window.onkeyup = commandsOneOff;
 
      //---------------------------------
 
      requestAnimFrame(animate);
    }
 
-   function animate() {
-     requestAnimFrame(animate);
+   function commands(event) {
+     console.log('keyup event detected!', event);
+     movies.current.visible = false;
+     movies.current.stop();
 
-     var animationAgeInMs = new Date().getTime();
+     switch (event.keyIdentifier) {
 
+       case 'Down':
+         movies.current = movies.crouch;
+         movies.crouch.loop = false;
+         movies.crouch.visible = true;
+         movies.crouch.play();
+         break;
+
+       case 'Left':
+         movies.current = movies.walk;
+         movies.walk.direction=0;
+         movies.walk.visible = true;
+         movies.walk.play();
+         break;
+
+       case 'Right':
+         movies.current = movies.walk;
+         movies.walk.direction=1;
+         movies.walk.visible = true;
+         movies.walk.play();
+         break;
+
+       default:
+         movies.current.visible = true;
+         movies.current.play();
+         break;
+     }
+   }
+
+   function commandsOneOff(event) {
+     console.log('keyup event detected!', event);
 
      for (var key in movies) {
        var movie = movies[key];
-       //var frame = (Math.floor(animationAgeInMs / msPerFrame) % textures[movie].length);
-       // movie.gotoAndStop(frame);
-        // movie.play();
+       movie.stop();
+       movie.visible = false;
+       movie.position.x = movies.current.x;
+       movie.position.y = movies.current.position.y;
      }
 
+     switch (event.keyIdentifier) {
 
-     // shoryuken.gotoAndStop(Math.floor(animationAgeInMs / msPerFrame) % textures.shoryuken.length);
-     // //shoryuken.position.y -= 0.5;
+       case 'Down':
+         movies.crouch.visible = false;
+         movies.crouch.gotoAndStop(0);
+         reset();
+         break;
+       case 'Up':
+         movies.current = movies.jump;
+         movies.jump.direction = 1;
+         movies.jump.loop = false;
+         movies.jump.visible = true;
+         movies.jump.play();
+         movies.jump.onComplete = reset;
+         break;
+       case 'Left':
+       case 'Right':
+         movies.crouch.visible = false;
+         movies.crouch.gotoAndStop(0);
+         reset();
+         break;
+       case 'U+0050': //"p"
+         movies.current = movies.punch;
+         movies.punch.loop = false;
+         movies.punch.visible = true;
+         movies.punch.play();
+         movies.punch.onComplete = reset;
+         break;
+       case 'U+004B': //"k"
+         movies.current = movies.kick;
+         movies.kick.loop = false;
+         movies.kick.visible = true;
+         movies.kick.play();
+         movies.kick.onComplete = reset;
+         break;
+       case 'U+0052': //"r"
+         movies.current = movies.roundhouse;
+         movies.roundhouse.loop = false;
+         movies.roundhouse.visible = true;
+         movies.roundhouse.play();
+         movies.roundhouse.onComplete = reset;
+         break;
+       case 'U+0048': //"h"
+         movies.current = movies.hadoken;
+         movies.hadoken.gotoAndStop(0);
+         movies.hadoken.loop = false;
+         movies.hadoken.visible = true;
+         movies.hadoken.play();
+         // movies.hadoken.onComplete = reset;
+         break;
+       case 'U+0053': //"s"
+         movies.current = movies.shoryuken;
+         movies.shoryuken.loop = false;
+         movies.shoryuken.visible = true;
+         movies.shoryuken.play();
+         movies.shoryuken.onComplete = reset;
+         break;
+       case 'U+0054': //"t"
+         movies.current = movies.tatsumaki;
+         movies.tatsumaki.loop = false;
+         movies.tatsumaki.visible = true;
+         movies.tatsumaki.play();
+         movies.tatsumaki.onComplete = reset;
+         break;
+       default:
+         reset();
+         break;
+     }
+   }
 
-     // tatsumaki.gotoAndStop((Math.floor(animationAgeInMs / msPerFrame) % textures.tatsumaki.length));
-     // //tatsumaki.position.x -= 0.5;
+   function reset() {
+     movies.current = movies.current || movies.stance;
+     //---
+     movies.current.visible = false;
+     movies.current.gotoAndStop(0);
+     //---
+     movies.current = movies.stance;
+     movies.current.visible = true;
+     movies.current.position.y = 200;
+     movies.current.play();
+   }
+
+   function addMovie(key, frames, prefix) {
+     textures[key] = [];
+     prefix = prefix || key;
+     for (var frame in frames) {
+       textures[key].push(prefix + '_' + pad(frames[frame], 2) + '.png');
+     }
+     movies[key] = PIXI.MovieClip.fromFrames(textures[key]);
+   }
+
+   function animate() {
+     requestAnimFrame(animate);
+
+     if (movies.jump.visible) {
+       if (movies.jump.position.y < 130 && movies.jump.direction === 1) {
+         movies.jump.direction = 0;
+       }
+       movies.jump.position.y += movies.jump.direction === 1 ? -4 : 4;
+     }
+
+     if (movies.walk.visible && movies.walk.direction === 1) {
+       movies.walk.position.x += movies.walk.position.x >= (384 - movies.walk.width) / 2 ? 0 : 2;
+     }
+
+     if (movies.walk.visible && movies.walk.direction === 0) {
+       movies.walk.position.x -= movies.walk.position.x <= 0 ? 0 : 2;
+     }
+
+     if (!movies.fireball.visible && movies.hadoken.visible && movies.hadoken.currentFrame === 2) {
+       movies.fireball.position.y = movies.current.position.y - 35;
+       movies.fireball.position.x = movies.current.position.x + 50;
+       movies.fireball.visible = true;
+       movies.fireball.play();
+     }
+
+     if (movies.fireball.visible) {
+       movies.fireball.position.x += 5;
+       if (movies.fireball.position.x >= 384 - movies.fireball.width) {
+         movies.fireball.visible = false;
+         movies.fireball.stop();
+         reset();
+       }
+     }
 
      renderer.render(stage);
    }
